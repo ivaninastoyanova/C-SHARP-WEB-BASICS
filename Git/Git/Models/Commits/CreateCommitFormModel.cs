c9ced027fd61f93a8cc1c90ddbd05be75fec2a11
ivaninastoyanova <72800115+ivaninastoyanova@users.noreply.github.com>
2022-02-18 23:27:54 +0200

    Add exam preparation

---
+++ Git/Git/Models/Commits/CreateCommitFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Models.Commits
+{
+    public class CreateCommitFormModel
+    {
+        public string Description { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ Git/Git/Models/Repositories/RepositoryListingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Models.Repositories
+{
+    public class RepositoryListingViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; init; }
+        public string Owner { get; init; }
+        public string CreatedOn { get; init; }
+        public int Commits { get; init; }
+    }
+}
